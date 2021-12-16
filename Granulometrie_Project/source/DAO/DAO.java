a594130186195d20775e80346170d8ac2091e737
noebr <noebr@172.25.4.82>
2021-12-16 17:25:23 +0100

    ChangementDeBase

--- Granulometrie_Project/source/DAO/DAO.java
+++ Granulometrie_Project/source/DAO/DAO.java
@@ -10,128 +10,127 @@ import javafx.scene.image.Image;
  */
 public class DAO {
 
-    /**
-     * @param url 
-     * @param login 
-     * @param password
-     */
-    private DAO(String  url, String  login, String password) {
-        // TODO implement here
-    }
-    /**
-     * 
-     */
-    private String url;
+	/**
+	 * @param url
+	 * @param login
+	 * @param password
+	 */
+	private DAO(String url, String login, String password) {
+		// TODO implement here
+	}
 
-    /**
-     * 
-     */
-    private String login;
+	/**
+	 * 
+	 */
+	private String url;
 
-    /**
-     * 
-     */
-    private String password;
+	/**
+	 * 
+	 */
+	private String login;
 
-    /**
-     * 
-     */
-    private Connection connection;
+	/**
+	 * 
+	 */
+	private String password;
 
+	/**
+	 * 
+	 */
+	private Connection connection;
 
+	/**
+	 * @param data
+	 */
+	public void insertData(GranuloData data) {
+		// TODO implement here
+	}
 
-    /**
-     * @param data
-     */
-    public void insertData(GranuloData data) {
-        // TODO implement here
-    }
+	/**
+	 * 
+	 */
+	public void getDB() {
+		// TODO implement here
+	}
 
-    /**
-     * 
-     */
-    public void getDB() {
-        // TODO implement here
-    }
+	/**
+	 * 
+	 */
+	public void getData() {
+		// TODO implement here
+	}
 
-    /**
-     * 
-     */
-    public void getData() {
-        // TODO implement here
-    }
+	/**
+	 * 
+	 */
+	public void getTableImages() {
+		// TODO implement here
+	}
 
-    /**
-     * 
-     */
-    public void getTableImages() {
-        // TODO implement here
-    }
+	/**
+	 * 
+	 */
+	public void getTableGrains() {
+		// TODO implement here
+	}
 
-    /**
-     * 
-     */
-    public void getTableGrains() {
-        // TODO implement here
-    }
+	/**
+	 * 
+	 */
+	public void getTableParametrage() {
+		// TODO implement here
+	}
 
-    /**
-     * 
-     */
-    public void getTableParametrage() {
-        // TODO implement here
-    }
+	/**
+	 * 
+	 */
+	public void getTableFamille() {
+		// TODO implement here
+	}
 
-    /**
-     * 
-     */
-    public void getTableFamille() {
-        // TODO implement here
-    }
+	/**
+	 * @param image
+	 */
+	public void getData(Image image) {
+		// TODO implement here
+	}
 
-    /**
-     * @param image
-     */
-    public void getData(Image image) {
-        // TODO implement here
-    }
+	/**
+	 * @param index
+	 */
+	public void getData(int index) {
+		// TODO implement here
+	}
 
-    /**
-     * @param index
-     */
-    public void getData(int index) {
-        // TODO implement here
-    }
+	/**
+	 * @param index
+	 */
+	public void deleteData(int index) {
+		// TODO implement here
+	}
 
-    /**
-     * @param index
-     */
-    public void deleteData(int index) {
-        // TODO implement here
-    }
+	/**
+	 * @param data
+	 * @param image
+	 */
+	public void insertParametrage(GranuloData data, Image image) {
+		// TODO implement here
+	}
 
-    /**
-     * @param data 
-     * @param image
-     */
-    public void insertParametrage(GranuloData data, Image image) {
-        // TODO implement here
-    }
+	/**
+	 * @param Image
+	 * @param idParametrege
+	 */
+	public void DeleteParametrage(Image images, int idParametrege) {
+		// TODO implement here
+	}
 
-    /**
-     * @param Image 
-     * @param idParametrege
-     */
-    public void DeleteParametrage(Image images, int idParametrege) {
-        // TODO implement here
-    }
-
-    /**
-     * @param Image 
-     * @param idParametrage
-     */
-    public void UpdateParametrage(Image Image, int idParametrage) {
-        // TODO implement here
-    }
+	/**
+	 * @param Image
+	 * @param idParametrage
+	 */
+	public void UpdateParametrage(Image Image, int idParametrage) {
+		// TODO implement here
+	}
 
 }
