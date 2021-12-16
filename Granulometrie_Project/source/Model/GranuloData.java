68c9846311f71ecf17895b268d39f482c3fc7a14
noebr <noebr@172.25.4.82>
2021-12-17 00:08:46 +0100

    javadoc GranuloData, contrcuteur, attribut, process(), setScale(),setClusters()

--- Granulometrie_Project/source/Model/GranuloData.java
+++ Granulometrie_Project/source/Model/GranuloData.java
@@ -12,52 +12,67 @@ import javafx.scene.image.Image;
 public class GranuloData {
 	 /**  
      * @param image
+     * Constructor of GranuloData object,
+     * it instanciate Measures with process(image) and image with this.Image
+     * Date and Time will be instanciate with the current date & time
+     *     
      */
     public GranuloData(Image image) {
         // TODO implement here
     }
     /**
-     * 
+     * Measure list, one grain specifities for each Measure
      */
     private List<Measure> Measures;
 
     /**
-     * 
+     *minimum size  of a grain for traitement (for result scaling )
      */
     private Float grainMin;
 
     /**
-     * 
+     *maximum size  of a grain for traitement (for result scaling )
      */
     private Float grainMax;
 
     /**
-     * 
+     * User's image to process
      */
     private Image image;
 
     /**
-     * 
+     * user comment of image 
      */
     private String comment;
 
     /**
-     * 
+     * current Date
      */
     private String Date;
 
     /**
-     * 
+     * current Time
      */
     private String Time;
 
     /**
-     * 
+     * Clusters contain a List of a Measure List with index, allows to classify the measures according with some conditions
      */
-    private HashMap<Integer, Measure> Cluster;
+    private HashMap<Integer, List<Measure>> Clusters;
 
    
-
+    /**
+     * @param image
+     * @return the measures list after the image Traitement (CCLabeler.getMeasure)
+     * 
+     * the methods store an image in ImageToProcessList object. It addImageName(image.getUrl()) will put this in ImageToProcessList's LinkedList
+     * after that, the methods create an CCLabeler object. Then, for each Image in the list, it past the images url in the CCLabeler. 
+     * Finally it return CCLabeler.getMeasure
+     * 
+     */
+    public List<Measure> process(Image image) {
+		return Measures;
+    }
     /**
      * @param min 
      * @param max
@@ -67,24 +82,15 @@ public class GranuloData {
     }
 
     /**
-     * 
+     * Create a HashMap<Integer, Measure> Cluster and set Scale of each Cluster (index & List<Measure>) according to Grains's size 
+     * (if float=2.0, then list<Mesuse> with index=0 will contain Grains Measure size beetween 0 and <2.0. index=1 Grains Measure size beetween 2.0 & <4.0 ...ect)
      */
-    public void setCluster() {
+    public void setClusters(float a) {
         // TODO implement here
     }
-
-    /**
-     * 
-     */
-    public void setComment() {
-        // TODO implement here
-    }
-
-    
-    
-    
     
     // Getter & Setter 
+    
 	/**
 	 * @return the measures
 	 */
@@ -186,8 +192,8 @@ public class GranuloData {
 	/**
 	 * @return the cluster
 	 */
-	public HashMap<Integer, Measure> getCluster() {
-		return Cluster;
+	public HashMap<Integer, List<Measure>> getClusters() {
+		return Clusters;
 	}
 
 }
