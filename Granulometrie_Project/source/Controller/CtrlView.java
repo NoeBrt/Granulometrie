351434fc0252c9af12642e89cc0f755b46331945
noebr <noebr@172.25.4.82>
2021-12-16 17:27:53 +0100

    chnagelen

--- Granulometrie_Project/source/Controller/CtrlView.java
+++ Granulometrie_Project/source/Controller/CtrlView.java
@@ -1,15 +1,9 @@
 package Controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.*;
 import javafx.scene.input.DragEvent;
-import javafx.fxml.FXML;
-
 /**
  * 
  */
