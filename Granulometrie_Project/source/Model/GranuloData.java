8c8ee00b0fe73e930e7aa2e01f80a4c6f8216b17
noebr <noebr@172.25.4.82>
2021-12-16 20:46:50 +0100

    Generate getter & setters

--- Granulometrie_Project/source/Model/GranuloData.java
+++ Granulometrie_Project/source/Model/GranuloData.java
@@ -80,4 +80,114 @@ public class GranuloData {
         // TODO implement here
     }
 
+    
+    
+    
+    
+    // Getter & Setter 
+	/**
+	 * @return the measures
+	 */
+	public List<Measure> getMeasures() {
+		return Measures;
+	}
+
+	/**
+	 * @param measures the measures to set
+	 */
+	public void setMeasures(List<Measure> measures) {
+		Measures = measures;
+	}
+
+	/**
+	 * @return the grainMin
+	 */
+	public Float getGrainMin() {
+		return grainMin;
+	}
+
+	/**
+	 * @param grainMin the grainMin to set
+	 */
+	public void setGrainMin(Float grainMin) {
+		this.grainMin = grainMin;
+	}
+
+	/**
+	 * @return the grainMax
+	 */
+	public Float getGrainMax() {
+		return grainMax;
+	}
+
+	/**
+	 * @param grainMax the grainMax to set
+	 */
+	public void setGrainMax(Float grainMax) {
+		this.grainMax = grainMax;
+	}
+
+	/**
+	 * @return the image
+	 */
+	public Image getImage() {
+		return image;
+	}
+
+	/**
+	 * @param image the image to set
+	 */
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	/**
+	 * @return the comment
+	 */
+	public String getComment() {
+		return comment;
+	}
+
+	/**
+	 * @param comment the comment to set
+	 */
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	/**
+	 * @return the date
+	 */
+	public String getDate() {
+		return Date;
+	}
+
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(String date) {
+		Date = date;
+	}
+
+	/**
+	 * @return the time
+	 */
+	public String getTime() {
+		return Time;
+	}
+
+	/**
+	 * @param time the time to set
+	 */
+	public void setTime(String time) {
+		Time = time;
+	}
+
+	/**
+	 * @return the cluster
+	 */
+	public HashMap<Integer, Measure> getCluster() {
+		return Cluster;
+	}
+
 }
