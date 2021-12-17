0ca04a2805a82984dccc0ecb977e8418bcdbd1eb
noebr <noebr@172.25.4.82>
2021-12-17 16:10:08 +0100

    javadoc SingleCOnnection

--- Granulometrie_Project/source/DAO/SingleConnection.java
+++ Granulometrie_Project/source/DAO/SingleConnection.java
@@ -6,8 +6,20 @@ import java.sql.SQLException;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 public class SingleConnection {
+	/**
+     * 
+     */
 	private static Connection con;
-
+	/**
+	 * SingleConnection makes a connection with the Database 
+	 * using a login and a password
+     * @param url : Database URL
+     * @param login : Login for the Database
+     * @param pwd : Password for the database
+     * This method will try to connect to the database named 
+     * localhost using these parameters, and return an 
+     * exception SQLException if the connection failed
+     */
 	private SingleConnection(String url, String login, String pwd) throws ClassNotFoundException {
 		try {
 
@@ -26,7 +38,15 @@ public class SingleConnection {
 		}
 
 	}
-
+	/**
+	 * Connection call the private method SingleConnection
+     * @param url : Database URL
+     * @param login : Login for the Database
+     * @param pwd : Password for the database
+     * This method will call SingleConnection with these
+     * parameters as methode's parameters, and return the 
+     * value of con
+     */
 	public static Connection getInstance(String url, String login, String pwd) throws ClassNotFoundException {
 		if (con == null) {
 			new SingleConnection(url, login, pwd) ;
