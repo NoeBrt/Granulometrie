d591c1d6bddbc48cd317c7064697a75111e20325
noebr <noebr@172.25.4.82>
2021-12-17 16:43:38 +0100

    add test

--- Granulometrie_Project/source/test/GranuloDataTest.java
+++ Granulometrie_Project/source/test/GranuloDataTest.java
@@ -1,5 +1,42 @@
 package test;
 
-public class GranuloDataTest {
+
+import org.junit.jupiter.api.Test;
+
+import javafx.scene.image.Image;
+
+class granuloDataTest {
+
+	/**
+	 * This method will test The method process
+	 * We will test it by comparing  with the data the method has returned with the correct data.
+	 * @param image
+	 */
+	@Test
+	public void TestProcess(Image image) {
+		
+	}
+	/**
+	 * This method will test The method setScale.
+	 * This method will test if the scale has been set.
+	 * @param min
+	 * @param max
+	 */
+	
+	@Test
+	public void TestsetScale(float min, float max) {
+        // TODO implement here
+    }
+	
+	/**
+	 * This method will test the method setClusters 
+	 * This method will test if the cluster has been set.
+	 * @param etalon
+	 */
+
+	@Test
+    public void TestsetClusters(float etalon) {
+        // TODO implement here
+    }
 
 }
