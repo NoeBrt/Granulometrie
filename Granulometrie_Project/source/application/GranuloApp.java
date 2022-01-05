a60120f6eb9a90fbf2a231f36e9f72db2f8dbb84
noebr <noebr@LAPTOP-AM7UMCFP.home>
2022-01-05 18:09:52 +0100

    app

--- Granulometrie_Project/source/application/GranuloApp.java
+++ Granulometrie_Project/source/application/GranuloApp.java
@@ -1,15 +1,16 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 public class GranuloApp extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Granulo.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("granulo.fxml").toExternalForm());
 			primaryStage.setScene(scene);
