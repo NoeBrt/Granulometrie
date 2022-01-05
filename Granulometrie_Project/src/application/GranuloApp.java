48e805e38d9cebce01539372d1a73d58deafb646
noebr <noebr@LAPTOP-AM7UMCFP.home>
2022-01-05 23:44:05 +0100

    modif fxml & app

--- Granulometrie_Project/src/application/GranuloApp.java
+++ Granulometrie_Project/src/application/GranuloApp.java
@@ -1,20 +1,26 @@
 package application;
 
+
+import java.net.URL;
+
+import Controller.CtrlView;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 public class GranuloApp extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = FXMLLoader.load(getClass().getResource("Granulo.fxml"));
-			//AnchorPane root = new AnchorPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader GranuloVue1 = new FXMLLoader(CtrlView.class.getResource("/Controller/GranuloVue1.fxml"));			 
+			BorderPane root = GranuloVue1.load();
+			Scene scene = new Scene(root,800,600);
 			primaryStage.setScene(scene);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.getIcons().add(new Image("/IconApp/icon.jpg"));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
