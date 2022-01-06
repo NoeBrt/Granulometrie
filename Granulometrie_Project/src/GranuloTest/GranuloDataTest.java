9d13711c02bf5e50977918caf053b2f153a253be
noebr <noebr@LAPTOP-AM7UMCFP.home>
2022-01-07 00:29:02 +0100

    granulotest

---
+++ Granulometrie_Project/src/GranuloTest/GranuloDataTest.java
@@ -0,0 +1,52 @@
+package GranuloTest;
+
+import org.junit.Test;
+
+import Model.GranuloData;
+import app.CCLabeler;
+import junit.framework.TestCase;
+
+class granuloDataTest extends TestCase {
+
+	/**
+	 * This method will test The method process We will test it by comparing with
+	 * the data the method has returned with the correct data.
+	 * 
+	 * @param image
+	 */
+	@Test
+	public void TestContructor() {
+		String url = "/Granulometrie_Project/ImageParticule/113_x63_zoom08_1.jpg";
+		GranuloData granulodata = new GranuloData(url);
+		CCLabeler counter = new CCLabeler();
+		counter.process(url);
+		assertEquals(counter.getMeasures(), granulodata.getMeasures());
+
+	}
+
+	/**
+	 * This method will test The method setScale. This method will test if the scale
+	 * has been set.
+	 * 
+	 * @param min
+	 * @param max
+	 */
+
+	@Test
+	public void TestsetScale() {
+		// TODO implement here
+	}
+
+	/**
+	 * This method will test the method setClusters This method will test if the
+	 * cluster has been set.
+	 * 
+	 * @param etalon
+	 */
+
+	@Test
+	public void TestsetClusters() {
+		// TODO implement here
+	}
+
+}
