b4a4bfba7a7afd2694e4a3e98d293e2b4bf53f2c
noebr <noebr@LAPTOP-AM7UMCFP.home>
2022-01-06 14:23:41 +0100

    write csv

---
+++ Granulometrie_Project/src/CSV/WriteCsv.java
@@ -0,0 +1,93 @@
+package CSV;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import Model.GranuloData;
+import app.Measure;
+
+public class WriteCsv {
+	private static String[] header;
+	private static String path;
+	private static GranuloData data;
+
+
+	/**
+	 * @param path String,name String,data List<Measure>,header
+	 * */
+
+
+	@SuppressWarnings("static-access")
+	public WriteCsv(GranuloData data, String[] header){
+		this.setData(data);
+		this.setHeader(header);
+		this.setPath("C:\\Documents\\result.csv");
+	}
+	/*
+	 * It initialize the Header gived by the constructor params and set in the local Attribute header then it write it in the CSV file 
+	 * then for each element in the List<Measures> data it write it in the same lign but different column  
+	 * */
+	public void StartWriting() throws IOException {  
+		FileWriter fileWriter = null;
+		try {
+			fileWriter = new FileWriter(getPath());
+			String header = "";
+			for (int i = 0; i < getHeader().length; i++) {
+				if(i == getHeader().length-1) {
+					header = header + getHeader()[i] + "\n";
+				}
+				else {
+					header = header +getHeader()[i] + ", ";
+				}
+			}
+			fileWriter.append(header);
+			for(Measure u: getData().getMeasuresAfterScale()) {
+				fileWriter.append(String.valueOf(u.getAire()));
+				fileWriter.append(",");
+				fileWriter.append(String.valueOf(u.getCentre_x()));
+				fileWriter.append(",");
+				fileWriter.append(String.valueOf(u.getCentre_y()));
+				fileWriter.append(",");
+				fileWriter.append(String.valueOf(u.getXstart()));
+				fileWriter.append(",");
+				fileWriter.append(String.valueOf(u.getYstart()));
+				fileWriter.append(",");
+				fileWriter.append(String.valueOf(u.getWidth()));
+				fileWriter.append(",");
+				fileWriter.append(String.valueOf(u.getHeight()));
+				fileWriter.append("\n");
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				fileWriter.flush();
+				fileWriter.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+
+
+	public static GranuloData getData() {
+		return data;
+	}
+	public void setData(GranuloData data) {
+		WriteCsv.data = data;
+	}
+	public static String[] getHeader() {
+		return header;
+	}
+	public void setHeader(String[] header) {
+		WriteCsv.header = header;
+	}
+	public static String getPath() {
+		return path;
+	}
+	public static void setPath(String path) {
+		WriteCsv.path = path;
+	}
+}
