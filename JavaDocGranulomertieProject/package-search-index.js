a0044d523196064b12fb31719369b397914aeb52
noebr <noebr@LAPTOP-AM7UMCFP.home>
2022-01-11 18:53:02 +0100

    delete main javadoc

--- JavaDocGranulomertieProject/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"app"},{"l":"application"},{"l":"Controller"},{"l":"CSV"},{"l":"DAO"},{"l":"GranuloTest"},{"l":"Model"}];updateSearchResults();
