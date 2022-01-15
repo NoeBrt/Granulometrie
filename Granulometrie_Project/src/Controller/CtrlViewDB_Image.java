86caf91863cd9718811f22aaf5be7eabcd38b41a
noebr <noebr@LAPTOP-AM7UMCFP.home>
2022-01-15 04:32:41 +0100

    correction

--- Granulometrie_Project/src/Controller/CtrlViewDB_Image.java
+++ Granulometrie_Project/src/Controller/CtrlViewDB_Image.java
@@ -18,15 +18,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TablePosition;
-import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 /**
